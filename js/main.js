aed6e859474ced8656194a69da0c1c3ce9ff8a12
muhammadBobur0 <muhammadboburbek@gmail.com>
2022-11-22 13:26:58 +0500

    news

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ elForm.addEventListener("submit", function (evt) {
 elList.addEventListener('click', function(evt){
   let evtid = evt.target.id
   if(evt.target.matches('.delete-btn')){
-    fetch('https://todos1-0-0.herokuapp.com/todos' , {
+    fetch('https://cors-anywhere.herokuapp.com/https://todos1-0-0.herokuapp.com/todos' , {
     method: 'DELETE',
     body: {
       id: evtid
